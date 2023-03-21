da5efff034892e0edb86fc0c170dd317d1151d9f
Nico Martinucci <domenico.martinucci@gmail.com>
2023-03-21 15:36:20 -0700

    add draft Footer component

--- react-app/src/Footer.js
+++ react-app/src/Footer.js
@@ -1,9 +1,41 @@
+import { ReactComponent as FacebookLogo } from "./social/facebook-white.svg";
+import { ReactComponent as InstagramLogo } from "./social/instagram-white.svg";
+import { ReactComponent as TwitterLogo } from "./social/twitter-white.svg";
+import { ReactComponent as AppStoreLogo } from "./store/app-store.svg";
+import { ReactComponent as PlayStoreLogo } from "./store/play-store.svg";
+import { ReactComponent as WindowsStoreLogo } from "./store/windows-store.svg";
+import "./Footer.css"
+
 /**
  * TODO:
  */
 function Footer() {
     return (
-        <h1>Footer</h1>
+        <div className="Footer-background">
+            <div className="Footer-linksDiv">
+                <a className="Footer-link" href="#">Home</a>|
+                <a className="Footer-link" href="#">Terms and Conditions</a>|
+                <a className="Footer-link" href="#">Privacy Policy</a>|
+                <a className="Footer-link" href="#">Collection Statement</a>|
+                <a className="Footer-link" href="#">Help</a>|
+                <a className="Footer-link" href="#">Manage Account</a>
+            </div>
+            <span className="Footer-copyright">
+                Copyright @ 2016 DEMO Streaming All Rights Reserved
+            </span>
+            <div className="Footer-iconsDiv">
+                <div className="Footer-socialMedia">
+                    <FacebookLogo className="Footer-socialLogo" />
+                    <TwitterLogo className="Footer-socialLogo" />
+                    <InstagramLogo className="Footer-socialLogo" />
+                </div>
+                <div className="Footer-externalLinks">
+                    <AppStoreLogo className="Footer-storeLogo" />
+                    <PlayStoreLogo className="Footer-storeLogo" />
+                    <WindowsStoreLogo className="Footer-storeLogo" />
+                </div>
+            </div>
+        </div>
     )
 }
 
